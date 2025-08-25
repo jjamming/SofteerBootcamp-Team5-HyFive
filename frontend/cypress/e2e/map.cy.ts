fe3b524eda1b9cad91af283022bcf581b0a9b799
minjoengss <dotoriido@gmail.com>
2025-08-26 02:39:29 +0900

    feat: 카카오맵 하이라이트 비활성화 테스트 구현

--- frontend/cypress/e2e/map.cy.ts
+++ frontend/cypress/e2e/map.cy.ts
@@ -30,9 +30,15 @@ describe("카카오맵 Marker 테스트", () => {
     cy.get("img[title*='marker-end']").should("have.length.at.least", 1);
     cy.get("img[title*='marker-middle']").should("have.length.at.least", 1);
   });
+
+  it("탑승자의 Marker의 경우 클릭이 활성화된다", () => {
+    cy.get("img[title*='marker-middle']").should("have.length.at.least", 1);
+    cy.get("img[title*='marker-middle']").click();
+    cy.get("[data-testid='map-infowindow']").should("exist");
+  });
 });
 
-describe("카카오맵 Highlight 테스트", () => {
+describe("카카오맵 Highlight 활성화 테스트", () => {
   beforeEach(() => {
     interceptAll();
     cy.visit("/admin/book/schedule");
@@ -64,5 +70,51 @@ describe("카카오맵 Highlight 테스트", () => {
   it("마커 클릭 시 승차 위치에 InfoWindow가 렌더링된다", () => {
     cy.get("img[title*='marker-middle']").first().click();
     cy.get("[data-testid='map-infowindow']").should("exist");
+  });
+});
+
+describe("카카오맵 Marker 비활성화 테스트", () => {
+  beforeEach(() => {
+    interceptAll();
+    cy.visit("/admin/book/schedule");
+    cy.wait("@getScheduleList");
+    cy.wait("@getPassengerList");
+    cy.wait("@getNodeList");
+
+    cy.wait(1000); // SDK 로딩 대기
+  });
+
+  it("초기 Marker가 렌더링된다", () => {
+    cy.get("img[title*='marker-start']").should("have.length.at.least", 1);
+    cy.get("img[title*='marker-end']").should("have.length.at.least", 1);
+    cy.get("img[title*='marker-middle']").should("have.length.at.least", 1);
+  });
+});
+
+describe("카카오맵 Highlight 테스트", () => {
+  beforeEach(() => {
+    interceptAll();
+    cy.visit("/admin/book/schedule");
+    cy.wait("@getScheduleList");
+    cy.wait("@getPassengerList");
+    cy.wait("@getNodeList");
+    cy.wait(1000); // SDK 로딩 대기
+  });
+
+  it("탑승자 경로 보기 버튼 클릭 후 탑승자 하이라이트 취소 버튼 클릭시 일반 Marker가 갱신된다", () => {
+    cy.get("div[data-testid='passenger-btn']").click();
+    cy.get("li[data-testid='passenger-highlight-btn']").first().click();
+    cy.get("[data-testid='highlight-cancel-btn']").click();
+    cy.get("img[title*='marker-start']").should("have.length.at.least", 1);
+    cy.get("img[title*='marker-end']").should("have.length.at.least", 1);
+    cy.get("img[title*='marker-middle']").should("have.length.at.least", 1);
+  });
+
+  it("마커 클릭 후 탑승자 하이라이트 취소 버튼 클릭시 일반 Marker가 갱신된다", () => {
+    cy.get("img[title*='marker-middle']").first().click();
+    cy.get("[data-testid='highlight-cancel-btn']").click();
+    cy.get("img[title*='marker-start']").should("have.length.at.least", 1);
+    cy.get("img[title*='marker-end']").should("have.length.at.least", 1);
+    cy.get("img[title*='marker-middle']").should("have.length.at.least", 1);
   });
 });
